a4995fa14734da8e0e3b77070d8954c1a855c707
Shinya Furumata <shin0724ya@gmail.com>
2019-09-17 23:19:51 +0900

    いったん何も返さないようにする

--- app.js
+++ app.js
@@ -33,16 +33,18 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 // event handler
 function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
+  return Promise.resolve(null);
+
+  // if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
-    return Promise.resolve(null);
-  }
+    // return Promise.resolve(null);
+  // }
 
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text + "って言うんかい" };
+  // const echo = { type: 'text', text: event.message.text + "って言うんかい" };
 
   // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  // return client.replyMessage(event.replyToken, echo);
 }
 
 // listen on port
